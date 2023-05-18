042553df4bd794e97393b7c0f212ac25e4e592f9
dimplekosal <129039138+dimplekosal@users.noreply.github.com>
2023-05-18 16:38:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image
+    {
+        fabric.fromURL('BirthdayImage jpg',function (Img))block_image_object = Img;
+        block_image_height=block_image_width+10;
+        block_image_height=block_image_height+10;
+        document.getElementById("current_width").innerHTML=block_image_height
+        document.getElementById("current_width").inn	
+	
+}
+
+function playSound(){
+<button class="btn-warning btn-lg" onclick="playsound()">Dear mom </button>
+function playsound() x.play(){
+    
+}
+}
